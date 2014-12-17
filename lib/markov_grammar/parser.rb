fc1c8fba95835b13a41b628b2bac3820488e8801
CoralineAda <coraline@idolhands.com>
2014-12-16 23:55:20 -0600

    Splitting out a generator

--- lib/markov_grammar/parser.rb
+++ lib/markov_grammar/parser.rb
@@ -5,7 +5,20 @@ module MarkovGrammar
     attr_accessor :corpus, :parsed_corpus
 
     def initialize(corpus="")
-      self.corpus = corpus
+      self.corpus = corpus.split("\n")[0..50].join("\n")
+    end
+
+    def prepare
+      File.open("data/structures.txt", "w") do |file|
+        file.puts sentence_structures
+      end
+    end
+
+    def dictionary
+      return @dictionary if @dictionary
+      @dictionary ||= MarkyMarkov::TemporaryDictionary.new
+      @dictionary.parse_file('./data/structures.txt')
+      @dictionary
     end
 
     def split_corpus
@@ -21,7 +34,7 @@ module MarkovGrammar
     end
 
     def parts_of_speech
-      @parts_of_speech = Grammar::PartsOfSpeech::WORD_LIST.keys.map(&:to_s)
+      @parts_of_speech ||= Grammar::PartsOfSpeech::WORD_LIST.keys.map(&:to_s)
     end
 
     def noun_phrases
@@ -29,7 +42,7 @@ module MarkovGrammar
     end
 
     def adjectives
-      noun_phrases.map do |phrase|
+      @adjectives ||= noun_phrases.map do |phrase|
         split = phrase.split
         next if phrase.length == 1
         split[-2..0]
@@ -37,7 +50,7 @@ module MarkovGrammar
     end
 
     def nouns
-      noun_phrases.map do |phrase|
+      @nouns ||= noun_phrases.map do |phrase|
         split = phrase.split
         split[-1]
       end.flatten.uniq - adjectives
@@ -52,21 +65,48 @@ module MarkovGrammar
     end
 
     def bare_words
-      with_substitutions.split.map do |word|
-        word = word.gsub(/[^a-z]+$/i, '')
-        parts_of_speech.include?(word) ? '|' : word
+      @bare_words ||= with_initial_substitutions.map do |sentence|
+        sentence.split.map do |word|
+          word = word.gsub(/[^a-z]+$/i, '')
+          parts_of_speech.include?(word) ? '|' : word
+        end.join(' ')
       end
     end
 
-    def with_substitutions
-      substituted = split_corpus.join(' ').downcase
-      categorized_word_list.keys.each do |keyset|
-        re = Regexp.union((keyset).flatten.map{|w| /\b#{Regexp.escape(w)}[ed]*\b/i})
-        substituted.gsub!(re, categorized_word_list[keyset].to_s)
+    def with_initial_substitutions
+      @with_initial_substitutions ||= sentences.map do |sentence|
+        categorized_word_list.keys.each do |keyset|
+          re = Regexp.union((keyset).flatten.map{|w| /\b#{Regexp.escape(w)}[ed]*\b/i})
+          sentence.gsub!(re, categorized_word_list[keyset].to_s)
+        end
+        sentence.gsub!(/[^a-z]+$/i, '')
+        sentence.gsub!(/\b[a-z]+ly/, 'adverb')
+        sentence.gsub!(/\b[a-z]+ing/, 'verb')
+        sentence
       end
-      substituted.gsub!(/[^a-z]+$/i, '')
-      substituted.gsub!(/\b[a-z]+ly/, 'adverb')
-      substituted
+    end
+
+    def with_final_substitions
+      @with_final_substitions ||= begin
+        with_initial_substitutions.map do |sentence|
+          re = Regexp.union((nouns).flatten.compact.map{|w| /\b#{Regexp.escape(w)}\b/i})
+          sentence.gsub!(re, 'noun')
+          re = Regexp.union((adjectives).flatten.compact.map{|w| /\b#{Regexp.escape(w)}\b/i})
+          sentence.gsub!(re, 'adjective')
+        end
+      end
+    end
+
+    def sentences
+      @sentence ||= begin
+        text = corpus.gsub(/(#{Grammar::PartsOfSpeech::HONORIFICS * '|'})/i, '\1@@@')
+        text = text.split(/[\.\?\!\;\,] /)
+        text = text.map{|t| t.gsub('@@@', '')}
+      end
+    end
+
+    def sentence_structures
+      @sentence_structures ||= with_final_substitions.compact.uniq
     end
 
     def categorized_word_list
--- test.rb
+++ test.rb
@@ -1,13 +1,9 @@
 require 'markov_grammar'
 require 'pry'
 
-p = MarkovGrammar::Parser.new(File.read("data/corpus.txt"))
+g = MarkovGrammar.Generator.new
+g.prepare
 
-puts p.corpus
-
-#puts p.with_substitutions
-#puts "original: #{p.corpus}"
-#puts "parsed: #{p.parsed_corpus.join(' ')}"
-
-puts "nouns: #{p.nouns}"
-puts "adjectives: #{p.adjectives}"
+10.times do
+  puts g.sentence
+end
